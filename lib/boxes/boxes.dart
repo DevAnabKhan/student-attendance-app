fc42b6adcd8b08419feeee338da800cb79659949
Anab Khan <anabkhan8148@gmail.com>
2024-01-04 11:19:35 +0500

    Second Commit

--- lib/boxes/boxes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:hive/hive.dart';
-import 'package:student_attendence/hive/model_class.dart';
-
-class Boxes{
-  static Box<Student> getData()=>Hive.box<Student>("Student");
-  static Box<Attendance> getAttendanceData()=>Hive.box<Attendance>("Attendance");
-  static Box<Teacher> getTeacherData()=>Hive.box<Teacher>("Teacher");
-  static Box<Grade> getGradeData()=>Hive.box<Grade>("Grade");
-}
